b1b2e417707478f3805a769c6e237478d36e0230
kyungsam <rudtka12@gmail.com>
2022-08-02 01:14:07 +0900

    [4948]베르트랑공준실버2/

---
+++ kyungsam/week1/common/P10870_피보나치.java
@@ -0,0 +1,18 @@
+
+public class P10870_피보나치 {
+	public static int res;
+	public static int num1;
+	public static int num2;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(fibo(10));
+	}
+	
+	static int fibo(int num) {
+		if(num==0)  return 0;
+		else if(num==1) return 1;
+		
+		return  fibo(num-1)+ fibo(num-2);
+		
+	}
+}
